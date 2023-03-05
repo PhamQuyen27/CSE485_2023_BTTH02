66b077bbd1ece39eac01cc015540c3611212e42a
Hoàng Minh Quân <zedloikiem666@gmail.com>
2023-03-05 11:47:45 +0700

    update

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,13 +1,33 @@
 <?php
-include("services/ArticleService.php");
 
-class HomeController{
-    // Hàm xử lý hành động index
-    public function index(){
-        // Nhiệm vụ 1: Tương tác với Services/Models
-        $articelService = new ArticleService();
-        $articles = $articelService->getAllArticles();
-        // Nhiệm vụ 2: Tương tác với View
-        include("views/home/index.php");
+if(isset($_GET['action'])){
+     $action = $_GET['action'];
+ }
+ else{
+     $action = '';
+ }
+
+// $thatbai = array();
+
+ switch($action){
+     case 'trangchu':{   
+         $tbltenbang = "baiviet";
+         $data = $db->getAllData($tbltenbang); 
+         require_once('views/home/index.php');
+         break;
+     }
+     case 'detail':{
+        $ma_bviet = $_GET['ma_bviet'];
+        $tbltenbang = "baiviet INNER JOIN tacgia
+        ON tacgia.ma_tgia = baiviet.ma_tgia
+        INNER JOIN theloai
+        ON baiviet.ma_tloai = theloai.ma_tloai
+        where ma_bviet = $ma_bviet ";
+        $data = $db->getAllData($tbltenbang);   
+
+        require_once('views/home/detail.php');
+        break;
     }
-}
+    
+ }
+?>
