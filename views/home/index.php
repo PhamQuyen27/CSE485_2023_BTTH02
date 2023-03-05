fe4695d9e9122d504da809d1fc7ac957fcb7443b
Hoàng Minh Quân <zedloikiem666@gmail.com>
2023-03-05 14:22:48 +0700

    update

--- views/home/index.php
+++ views/home/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
@@ -18,21 +20,21 @@
                     </a>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php?controller=home&action=trangchu">Trang chủ</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="index.php?controller=home&action=login">Đăng nhập</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Nội dung cần tìm" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Tìm</button>
-                </form>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php?controller=home&action=trangchu">Trang chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?controller=home&action=login">Đăng nhập</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Nội dung cần tìm" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Tìm</button>
+                    </form>
                 </div>
             </div>
         </nav>
@@ -45,13 +47,13 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img src="assets/images/slideshow/slide01.jpg" class="d-block w-100" alt="...">
+                    <img src="assets/images/slideshow/slide01.jpg" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                <img src="assets/images/slideshow/slide02.jpg" class="d-block w-100" alt="...">
+                    <img src="assets/images/slideshow/slide02.jpg" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                <img src="assets/images/slideshow/slide03.jpg" class="d-block w-100" alt="...">
+                    <img src="assets/images/slideshow/slide03.jpg" class="d-block w-100" alt="...">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -62,34 +64,34 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
-            </div>
+        </div>
     </header>
     <main class="container-fluid mt-3">
         <h3 class="text-center text-uppercase mb-3 text-primary">TOP bài hát yêu thích</h3>
         <div class="row">
-        <?php
-        
-                foreach($data as $value){
-                    
+            <?php
+
+            foreach ($data as $value) {
+
             ?>
-            <div class="col-sm-3">
+                <div class="col-sm-3">
                     <div class="card mb-2" style="width: 100%;">
-                        <img src="<?php echo $value['hinhanh'];?>" class="card-img-top" alt="...">
+                        <img src="<?php echo $value['hinhanh']; ?>" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title text-center">
-                                <a href="index.php?controller=home&action=detail&ma_bviet=<?php echo $value['ma_bviet']?>" class="text-decoration-none"><?php echo $value['ten_bhat'];?></a>
+                                <a href="index.php?controller=home&action=detail&ma_bviet=<?php echo $value['ma_bviet'] ?>" class="text-decoration-none"><?php echo $value['ten_bhat']; ?></a>
                             </h5>
                         </div>
                     </div>
-            </div>
+                </div>
             <?php
-                }
-            ?> 
+            }
+            ?>
         </div>
     </main>
-    <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
-        <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <?php
+    require_once('views/layouts/footer.php');
+    ?>
 </body>
+
 </html>
